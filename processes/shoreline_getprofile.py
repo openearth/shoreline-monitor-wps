3e158df09090914547a88890af338a9dd7fe3063
hendriksen <gerrit.hendriksen@deltares.nl>
2025-09-30 15:43:07 +0200

    extra logging

--- processes/shoreline_getprofile.py
+++ processes/shoreline_getprofile.py
@@ -64,6 +64,7 @@ pg_pass = os.getenv("PG_PASS")
 pg_host = os.getenv("PG_HOST")
 pg_db = os.getenv("PG_DB")
 pg_port = 5432
+logger.info('host/username',pg_host,pg_user)
 
 engine = create_engine(
     f"postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
